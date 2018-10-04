a7f37655d21542fc88f8f0760e0dbda7d47e43f1
gkaguirre <gkaguirre@me.com>
2018-10-03 23:14:21 -0400

    First commit
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/submitHCPStructGears.m
@@ -0,0 +1,150 @@
+
+
+%% Fixed variable definition
+projectName = 'tome';
+freesurferLicenseFileName = 'freesurfer_license.txt';
+fileType = 'nifti';
+gearName = 'hcp-struct';
+rootSessionLabel = 'Session 1';
+paramsFileName = 'tomeHCPStructuralParams.csv';
+
+
+%% Load the params table
+% This identifies the subjects and inputs to be processed
+paramsTable = readtable(paramsFileName);
+nParamColumns = 3;
+nJobs = size(paramsTable,2)-nParamColumns;
+nInputs = size(paramsTable,1);
+
+
+%% Instantiate the flywheel object
+fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
+
+
+%% Get project ID and sessions
+allProjects = fw.getAllProjects;
+projIdx = find(strcmp(cellfun(@(x) x.label,allProjects,'UniformOutput',false),projectName),1);
+projID = allProjects{projIdx}.id;
+allSessions = fw.getProjectSessions(projID);
+
+
+%% Identify the freesurfer license file
+freesurferLicenseFileIdx = find(strcmp(cellfun(@(x) x.name,allProjects{projIdx}.files,'UniformOutput',false),freesurferLicenseFileName));
+freesurferLicenseFileID = allProjects{projIdx}.files{freesurferLicenseFileIdx}.id;
+
+
+%% Construct the gear configuration
+% Get all the gears
+allGears = fw.getAllGears();
+
+% Find the particular gear we are going to use
+thisGarIdx=find(strcmp(cellfun(@(x) x.gear.name,allGears,'UniformOutput',false),gearName));
+thisGearID = allGears{thisGarIdx}.id;
+
+% Build the config params. Read the config to set the defaults and edit
+% required ones
+gear = fw.getGear(thisGearID);
+gearCfg = struct(gear.gear.config);
+configDefault = struct;
+keys = fieldnames(gearCfg);
+for i = 1:numel(keys)
+  val = gearCfg.(keys{i});
+  if isfield(val, 'default')
+    configDefault.(keys{i}) = val.default;
+  else
+    fprintf('No default value for %s\n. It must be set prior to execution.', keys{i});
+  end
+end
+
+
+%% Loop through jobs
+for jj=1:nJobs
+
+    
+    %% Assemble Inputs
+    % Create an empty inputs struct
+    inputs = struct();
+    
+    % Loop through the inputs specified in the paramsTable
+    for ii=1:size(paramsTable,1)
+        
+        entry = strsplit(char(paramsTable{ii,jj+nParamColumns}),'/');
+        % Try to find the session with this subject and session label
+        sessionIdx = find(cellfun(@(x) all([strcmp(x.subject.code,entry{1}) strcmp(x.label,entry{2})]),allSessions));
+        if isempty(sessionIdx)
+            error('No matching session and subject for this input entry')
+        end
+        if length(sessionIdx)>1
+            error('More than one matching session and subject for this input entry')
+        end
+        % Obtain the set of acquisitions for the matching session
+        allAcqs = fw.getSessionAcquisitions(allSessions{sessionIdx}.id);
+        % Check to see if there is a custom file label. If not, use the
+        % default label
+        if length(entry)==3
+            targetLabel = entry(3);
+        else
+            targetLabel = char(paramsTable{ii,2});
+        end
+        % Check if this is a session file, or an acquisition file
+        if logical(paramsTable{ii,3})
+            % It is a session file (such as a coeff.grad file). See if the
+            % target file is there.
+            acqIdx = find(strcmp(cellfun(@(x) x.name,allSessions{sessionIdx}.files,'UniformOutput',false),targetLabel));
+            acqID = allSessions{sessionIdx}.files{acqIdx}.id;
+        else
+            % It is an acqusition file. Try to find an acquisition that
+            % matches the input label and contains a nifti file. We use
+            % strfind instead of strcmp, as the stored label in flywheel
+            % sometimes has a trailing space.
+            labelMatchIdx = cellfun(@(x) ~isempty(x),strfind(cellfun(@(x) x.label,allAcqs,'UniformOutput',false),targetLabel));
+            isNiftiIdx = cellfun(@(x) any(cellfun(@(y) strcmp(y.type,'nifti'),x.files)),allAcqs);
+            acqIdx = logical(labelMatchIdx .* isNiftiIdx);
+            if ~any(acqIdx)
+                error('No matching acquisition for this input entry')
+            end
+            if sum(acqIdx)>1
+                error('More than one matching acquisition for this input entry')
+            end
+            % We have a match. Get the ID
+            acqID = allAcqs{acqIdx}.id;
+        end
+        % Add this input information to the structure
+        inputStem = struct('type', 'acquisition',...
+                  'id', acqID, ...
+                  'name', targetLabel);
+        inputs.(char(paramsTable{ii,1})) = inputStem;
+    end
+
+    
+    %% Identify the root session
+    % This is the session to which the analysis product will be assigned
+    subjectName = char(paramsTable.Properties.VariableNames{jj+nParamColumns});
+    rootSessionIdx = find(cellfun(@(x) all([strcmp(x.subject.code,subjectName) strcmp(x.label,rootSessionLabel)]),allSessions));    
+    rootSessionID = allSessions{rootSessionIdx}.id;
+
+    
+    %% Customize gear configuration
+    config = configDefault;
+    config.Subject = subjectName;
+
+    
+    %% Assemble Job
+    % Create the job body with all the involved files in a struct
+    thisJob = struct('gear_id', thisGearID, ...
+        'inputs', inputs, ...
+        'config', config);
+
+    
+    %% Assemble analysis label
+    analysisLabel = [gearName ' v' allGears{thisGarIdx}.gear.version ' - ' char(datetime('now','TimeZone','local','Format','dd/MM/yyyy HH:mm:ss'))];
+
+    
+    %% Run
+    body = struct('label', analysisLabel, 'job', thisJob);
+    [returnData, resp] = fw.addSessionAnalysis(rootSessionID, body);
+    
+end
+
+
+
---
+++ config/mriTOMEAnalysisLocalHookTemplate.m
@@ -0,0 +1,68 @@
+function mrTOMEAnalysisLocalHook
+%  LFContrastAnalsysisLocalHook
+%
+% Configure things for working on the  mrTOMEAnalysis project.
+%
+% For use with the ToolboxToolbox.
+%
+% If you 'git clone' ILFContrastAnalsysis into your ToolboxToolbox "projectRoot"
+% folder, then run in MATLAB
+%   tbUseProject('mrTOMEAnalysis')
+% ToolboxToolbox will set up IBIOColorDetect and its dependencies on
+% your machine.
+%
+% As part of the setup process, ToolboxToolbox will copy this file to your
+% ToolboxToolbox localToolboxHooks directory (minus the "Template" suffix).
+% The defalt location for this would be
+%   ~/localToolboxHooks/LFContrastAnalsysisLocalHook.m
+%
+% Each time you run tbUseProject('mrTOMEAnalysis'), ToolboxToolbox will
+% execute your local copy of this file to do setup for LFContrastAnalsysis.
+%
+% You should edit your local copy with values that are correct for your
+% local machine, for example the output directory location.
+%
+
+
+%% Say hello.
+fprintf('mrTOMEAnalysis local hook.\n');
+projectName = 'mrTOMEAnalysis';
+
+%% Delete any old prefs
+if (ispref(projectName))
+    rmpref(projectName);
+end
+
+%% Specify base paths for materials and data
+[~, userID] = system('whoami');
+userID = strtrim(userID);
+switch userID
+    case {'dhb'}
+        materialsBasePath = ['/Users1' '/Dropbox (Aguirre-Brainard Lab)/TOME_materials'];
+        TOME_dataBasePath = ['/Users1' '/Dropbox (Aguirre-Brainard Lab)/TOME_data/'];     
+    case {'mbarnett'}
+        materialsBasePath = ['/home/mbarnett/Dropbox (Aguirre-Brainard Lab)/TOME_materials'];
+        TOME_dataBasePath = ['/home/mbarnett/Dropbox (Aguirre-Brainard Lab)/TOME_data/'];
+    otherwise
+        materialsBasePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/TOME_materials'];
+        TOME_dataBasePath = ['/Users/' userID '/Dropbox (Aguirre-Brainard Lab)/TOME_data/'];
+end
+
+%% Specify where output goes
+
+if ismac
+    % Code to run on Mac plaform
+    setpref(projectName,'analysisScratchDir','/tmp/flywheel');
+    setpref(projectName,'projectRootDir',fullfile('/Users/',userID,'/Documents/flywheel',projectName));
+    setpref(projectName,'TOMEDataPath', TOME_dataBasePath);
+elseif isunix
+    % Code to run on Linux plaform
+    setpref(projectName,'analysisScratchDir','/tmp/flywheel');
+    setpref(projectName,'projectRootDir',fullfile('/home/',userID,'/Documents/flywheel',projectName));
+    setpref(projectName,'TOMEDataPath', TOME_dataBasePath);
+elseif ispc
+    % Code to run on Windows platform
+    warning('No supported for PC')
+else
+    disp('What are you using?')
+end
