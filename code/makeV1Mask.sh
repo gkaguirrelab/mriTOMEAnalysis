88021e20f5c2e4ad4e42c5588748418a0c8bafdc
harrison-mcadams <harrison.mcadams@gmail.com>
2018-10-22 09:50:52 -0400

    added call for mri_vol2vol
    somehow i missed copying this over initially
    this commit also includes changing freeSurferDir to being an input to the function

--- code/makeV1Mask.sh
+++ code/makeV1Mask.sh
@@ -12,10 +12,11 @@ export FSLDIR PATH
 . ${FSLDIR}/etc/fslconf/fsl.sh
 
 # set up the FreeSurfer data lives
-export SUBJECTS_DIR=~/Downloads/TOME_3003/T1w
+freeSurferDir=$1
+export SUBJECTS_DIR=freeSurferDir
 
 bbregister --s TOME_3003 --mov ~/Downloads/TOME_3003/T1w/T1w1_gdc.nii.gz --reg ~/Desktop/register_bashTest.dat --t1 --init-fsl
 
 mri_label2vol --label ~/Downloads/TOME_3003/T1w/TOME_3003/label/lh.V1.label --temp ~/Downloads/TOME_3003/T1w/T1w1_gdc.nii.gz --o ~/Desktop/lh_v1_register.nii.gz --reg ~/Desktop/register.dat
 
-mri_label2vol --label ~/Downloads/TOME_3003/T1w/TOME_3003/label/lh.V1.label --temp ~/Downloads/TOME_3003/T1w/T1w1_gdc.nii.gz --o ~/Desktop/lh_v1_register.nii.gz --reg ~/Desktop/register.dat
+mri_vol2vol --mov ~/Desktop/lh_v1_register.nii.gz --targ ~/Downloads/TOME_3003_functional/rfMRI_REST_AP_Run1/rfMRI_REST_AP_Run1_gdc.nii.gz --o ~/Desktop/lh_v1_register_restAsTarg_identity_nearest.nii.gz --regheader --interp nearest
