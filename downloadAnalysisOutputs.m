d11ca1021a0a341f5071bc902e725b8670ff008e
gkaguirre <gkaguirre@me.com>
2018-10-31 17:12:56 -0400

    A simple script to save analysis outputs
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ downloadAnalysisOutputs.m
@@ -0,0 +1,39 @@
+% Download TOME hcp-struct analysis result to local directory
+
+projectName = 'tome';
+gearName = 'hcp-struct';
+rootSaveDir = '/Users/aguirre/Desktop/test';
+outputFileStem = '.hcpstruct_QC.inflated_MyelinMap_BC.png';
+
+%% Instantiate the flywheel object
+fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
+
+%% Find all analyses of the specified gear
+searchStruct = struct(...
+        'returnType', 'analysis', ...
+        'filters', {{ struct('wildcard', struct('analysis0x2elabel', ['*' gearName '*']))}} ...
+        );
+analyses = fw.search(searchStruct);
+
+%% Refine to just those in the specified project
+idx = cellfun(@(x) strcmp(projectName,x.project.label),analyses);
+analyses = analyses(idx);
+
+%% Loop through the analyses and download
+for ii = 1:numel(analyses)
+    
+    % Get the analysis object
+    thisAnalysis = fw.getAnalysis(analyses{ii}.analysis.id);
+
+    % Find the file with the matching stem
+    fileMatchIdx = cellfun(@(x) contains(x.name,outputFileStem),thisAnalysis.files);
+
+    % Have some sanity checking here to error if there are none or more
+    % than one matching files
+    %% TODO
+        
+    % Download the matching files to the rootSaveDir
+    thisName = thisAnalysis.files{fileMatchIdx}.name;
+    fw.downloadOutputFromAnalysis(thisAnalysis.id,thisName,fullfile(rootSaveDir,thisName));
+end
+
