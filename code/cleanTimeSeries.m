e1c5ad6d8d24e5f0f1b4b250e11b021eeecb0896
harrison-mcadams <harrison.mcadams@gmail.com>
2018-11-07 17:28:26 -0500

    more generalized solution to find the timebase
    stimulus and response can now have different timebases

--- code/cleanTimeSeries.m
+++ code/cleanTimeSeries.m
@@ -2,6 +2,7 @@ function [ cleanedTimeSeries ] = cleanTimeSeries( inputTimeSeries, regressors, v
 
 p = inputParser; p.KeepUnmatched = true;
 p.addParameter('TR',800, @isnumber);
+p.addParameter('totalTime',336000, @isnumber);
 p.addParameter('meanCenterRegressors', true, @islogical);
 p.parse(varargin{:});
 
@@ -15,18 +16,18 @@ thePacket.stimulus.values = [];
 thePacket.response.timebase = [];
 thePacket.response.values = [];
 
-% makig the timebase
-TR = p.Results.TR;
-deltaT = 0.8*1000;
-totalTime = 336*1000;
+% based on the number of samples, we can figure out the timebase
+totalTime = p.Results.totalTime;
+deltaT = totalTime/size(regressors,1);
+
 
 % this will serve as the timebase for both the response as well as the
 % regressors
-timebase = 0:deltaT:totalTime-TR;
+timebase = 0:deltaT:totalTime-deltaT;
 
 % add the timebases to the packets.
 thePacket.stimulus.timebase = timebase;
-thePacket.response.timebase = timebase;
+thePacket.response.timebase = 0:p.Results.TR:totalTime-p.Results.TR;
 
 % mean center the regressors, if asked
 regressors = regressors - nanmean(regressors);
@@ -51,7 +52,8 @@ for tt = 1:nTimeSeries
     
     % TFE linear regression here
     [paramsFit,fVal,modelResponseStruct] = temporalFit.fitResponse(thePacket,...
-        'defaultParamsInfo', defaultParamsInfo, 'searchMethod','linearRegression','errorType','1-r2');
+        'defaultParamsInfo', defaultParamsInfo, 'errorType','1-r2');
+ %       'defaultParamsInfo', defaultParamsInfo, 'searchMethod','linearRegression','errorType','1-r2');
     
     % remove signal related to regressors to yield clean time series
     cleanedTimeSeries(tt,:) = thePacket.response.values - modelResponseStruct.values;
