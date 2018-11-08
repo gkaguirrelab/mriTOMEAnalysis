efca8e01cd8277bbe85e2f7a260aee6049a82445
harrison-mcadams <harrison.mcadams@gmail.com>
2018-11-08 16:20:26 -0500

    batch script to loop around subjects and runs

---
+++ code/batch_analyzeRest.m
@@ -0,0 +1,38 @@
+function batch_analyzeRest
+
+fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
+
+searchStruct = struct(...
+    'returnType', 'file', ...
+    'filters', {{ ...
+    struct('wildcard', struct('analysis0x2elabel', '*hcp-struct*')), ...
+    struct('match', struct('project0x2elabel', 'tome')), ...
+    }} ...
+    );
+analyses = fw.search(searchStruct, 'size', '1000');
+
+for ii = 1:numel(analyses)
+    
+    if ~strcmp(analyses{ii}.file.name, [analyses{ii}.subject.code, '_hcpstruct.zip'])
+        analyses{ii} = [];
+    end
+end
+
+analyses = analyses(~cellfun('isempty', analyses));
+
+
+
+numberOfSubjects = size(analyses,1);
+restScans = {'rfMRI_REST_AP_Run1', 'rfMRI_REST_PA_Run2', 'rfMRI_REST_AP_Run3', 'rfMRI_REST_PA_Run4'};
+%% from each session, download the hcp-struct.zip
+for ss = 1:numberOfSubjects
+    for rr = 1:length(restScans)
+        
+        subjectID = analyses{ss}.subject.code;
+        runName = restScans{rr};
+        analyzeRest(subjectID, runName);
+        
+    end
+end
+
+end
