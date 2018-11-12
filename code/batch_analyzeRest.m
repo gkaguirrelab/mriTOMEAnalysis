6b21d405a69232ddf4232dfff5072956d4d59c3a
harrison-mcadams <harrison.mcadams@gmail.com>
2018-11-12 11:58:02 -0500

    try catch implemented

--- code/batch_analyzeRest.m
+++ code/batch_analyzeRest.m
@@ -1,5 +1,11 @@
 function batch_analyzeRest
 
+% set up error log
+errorLogPath = fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), '/mriTOMEAnalysis/');
+currentTime = clock;
+errorLogFilename = ['errorLog_', num2str(currentTime(1)), '-', num2str(currentTime(2)), '-', num2str(currentTime(3)), '_', num2str(currentTime(4)), num2str(currentTime(5))];
+system(['echo "', 'SubjectID', ',', 'runName', '" > ', [errorLogPath, errorLogFilename]]);
+
 fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
 
 searchStruct = struct(...
@@ -24,8 +30,10 @@ analyses = analyses(~cellfun('isempty', analyses));
 
 numberOfSubjects = size(analyses,1);
 %% from each session, download the hcp-struct.zip
-for ss = 1:numberOfSubjects
+for ss = 4:numberOfSubjects
     
+    subjectID = analyses{ss}.subject.code;
+
     searchStruct = struct(...
         'returnType', 'file', ...
         'filters', {{ ...
@@ -33,22 +41,22 @@ for ss = 1:numberOfSubjects
         struct('match', struct('project0x2elabel', 'tome')), ...
         }} ...
         );
-    analyses = [];
-    analyses = fw.search(searchStruct, 'size', '10000');
+    subjectAnalyses = [];
+    subjectAnalyses = fw.search(searchStruct, 'size', '10000');
     
-    for ii = 1:numel(analyses)
+    for ii = 1:numel(subjectAnalyses)
         
-        if ~strcmp(analyses{ii}.subject.code, subjectID) || ~contains(analyses{ii}.file.name, 'REST') || ~contains(analyses{ii}.file.name, 'hcpfunc.zip') || contains(analyses{ii}.file.name, 'log')
-            analyses{ii} = [];
+        if ~strcmp(subjectAnalyses{ii}.subject.code, subjectID) || ~contains(subjectAnalyses{ii}.file.name, 'REST') || ~contains(subjectAnalyses{ii}.file.name, 'hcpfunc.zip') || contains(subjectAnalyses{ii}.file.name, 'log')
+            subjectAnalyses{ii} = [];
         end
     end
     
-    analyses = analyses(~cellfun('isempty', analyses));
+    subjectAnalyses = subjectAnalyses(~cellfun('isempty', subjectAnalyses));
     
     runNames = [];
-    for ii = 1:length(analyses)
+    for ii = 1:length(subjectAnalyses)
         
-        wholeFileName = analyses{ii}.file.name;
+        wholeFileName = subjectAnalyses{ii}.file.name;
         wholeFileName_split = strsplit(wholeFileName, '_');
         runName = [wholeFileName_split{3}, '_', wholeFileName_split{4}, '_', wholeFileName_split{5}, '_', wholeFileName_split{6}];
         runNames{end+1} = runName;
@@ -57,11 +65,15 @@ for ss = 1:numberOfSubjects
     
     for rr = 1:length(runNames)
         
-        subjectID = analyses{ss}.subject.code;
         runName = runNames{rr};
+        
+        try
         analyzeRest(subjectID, runName);
         
         close all
+        catch
+            system(['echo "', subjectID, ',', runName, '" >> ', [errorLogPath, errorLogFilename]]);
+        end
         
     end
 end
