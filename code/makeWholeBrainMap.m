3b1d3459f9be37812b9aa4452bb3af20a956b4ec
harrison-mcadams <harrison.mcadams@gmail.com>
2018-12-17 16:50:14 -0500

    new function
    make volume with inputted statistics and template volume

--- code/makeWholeBrainMap.m
+++ code/makeWholeBrainMap.m
@@ -1,54 +1,27 @@
-function [] = makeWholeBrainMap(subjectID, runName, regressor, varargin)
+function [ statsVolume ] = makeWholeBrainMap(stats, voxelIndices, templateVolume)
 
-p = inputParser; p.KeepUnmatched = true;
-p.addParameter('freeSurferDir',fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), '/mriTOMEAnalysis/flywheelOutput/', subjectID, '/freeSurfer'),  @isstring);
-p.addParameter('anatDir',fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), '/mriTOMEAnalysis/flywheelOutput/', subjectID), @isstring);
-p.addParameter('pupilDir',fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), '/mriTOMEAnalysis/flywheelOutput/', subjectID), @isstring);
-p.addParameter('functionalDir',fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), '/mriTOMEAnalysis/flywheelOutput/', subjectID),  @isstring);
-p.addParameter('outputDir',fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), '/mriTOMEAnalysis/flywheelOutput/', subjectID), @isstring);
+%% Set up our output variable
+statsVolume = templateVolume;
+nStats = length(stats);
+statsVolume.vol = zeros(size(templateVolume.vol,1), size(templateVolume.vol,2), size(templateVolume.vol,3), nStats);
+clear templateVolume;
 
+%% checks on the inputted data
+% make sure the length of the stats vector is as long as the length of the
+% cell array voxel indices
+assert(length(stats), length(voxelIndices));
 
-p.parse(varargin{:});
-
-%% Load up the functional scan
-% we specifically want the functional scan that has already been brought
-% back to native anatomical space.
-% using try-catch stuff here in case dropbox needs to download the file
-functionalDir = p.Results.functionalDir;
-stillTrying = true; tryAttempt = 0;
-while stillTrying
-    try
-        system(['touch -a "', fullfile(functionalDir, [runName, '_native.nii.gz']), '"']);
-        pause(tryAttempt*60);
-        functionalScan = MRIread(fullfile(functionalDir, [runName, '_native.nii.gz']));
-        stillTrying = false;
-    catch
-        tryAttempt = tryAttempt + 1;
-        stillTrying = tryAttempt < 6;
-    end
+%% Loop over voxels, stashing the relevant results within the voxel
+for vv = 1:length(voxelIndices)
+    
+    % determine the coordiantes for the relevant voxel
+    xx = voxelIndices{vv}(1);
+    yy = voxelIndices{vv}(2);
+    zz = voxelIndices{vv}(3);
+    
+    % stash the result in that voxel
+    statsVolume.vol(xx,yy,zz,:) = stats(vv);
+    
 end
-
-%% Loop over voxels, and perform the IAMP fit
-
-% dimensions of our functional data
-nXIndices = size(functionalScan.vol, 1);
-nYIndices = size(functionalScan.vol, 2);
-nZIndices = size(functionalScan.vol, 3);
-nTRs = size(functionalScan.vol, 4);
-
-betaVolume = functionalScan;
-rSquaredVolume = functionalScan;
-betaVolume.vol = [];
-rSquaredVolume.vol = [];
-
-for xx = 1:nXIndices
-    for yy = 1:nYIndices
-        for zz = 1:nZIndices
-            voxelTimeSeries = functionalScan.vol(xx,yy,zz,:);
-            voxelTimeSeries = reshape(voxelTimeSeries,1,nTRs);
-        end
-    end
-end
-
 
 end
