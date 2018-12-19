ed2a9c19ad76351595e9df6e5a70d2282f6cd85f
harrison-mcadams <harrison.mcadams@gmail.com>
2018-12-19 10:40:58 -0500

    now for wholeBrain analysis

--- code/toRun.m
+++ code/toRun.m
@@ -1,8 +1,7 @@
-analyzeRest('TOME_3005', 'rfMRI_REST_AP_Run1');
-analyzeRest('TOME_3005', 'rfMRI_REST_PA_Run2');
-analyzeRest('TOME_3005', 'rfMRI_REST_AP_Run3');
-analyzeRest('TOME_3005', 'rfMRI_REST_PA_Run4');
+analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_AP_Run1');
+analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_PA_Run2');
+analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_AP_Run3');
+analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_PA_Run4');
 
-batch_analyzeRest;
 
 
