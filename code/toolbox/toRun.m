e6b6bf1f96c7b61b2149152af6bf0ac7741842e2
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-09 11:41:02 -0500

    new subject toRun

--- code/toolbox/toRun.m
+++ code/toolbox/toRun.m
@@ -1,7 +1,7 @@
-analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_AP_Run1');
-analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_PA_Run2');
-analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_AP_Run3');
-analyzeRest_wholeBrain('TOME_3005', 'rfMRI_REST_PA_Run4');
+analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1');
+analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2');
+analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3');
+analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4');
 
 
 
