cd5029c270585011eb52fdaed2d0b19e6a35aa68
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-10 14:43:04 -0500

    simplified to run faster

--- code/old/debugBeta.m
+++ code/old/debugBeta.m
@@ -1,6 +1,8 @@
 subjectID = 'TOME_3003';
 runName = 'rfMRI_REST_AP_Run3';
 functionalScan = MRIread('/Users/harrisonmcadams/Dropbox-Aguirre-Brainard-Lab/MELA_analysis/mriTOMEAnalysis/flywheelOutput/TOME_3003/tfMRI_FLASH_PA_run2_native.nii.gz');
+load('/Users/harrisonmcadams/Dropbox (Aguirre-Brainard Lab)/MELA_analysis/mriTOMEAnalysis/wholeBrain/resting/TOME_3003/rfMRI_REST_AP_Run1_voxelTimeSeries.mat')
+
 
 savePath = fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), 'mriTOMEAnalysis', 'wholeBrain', 'resting', subjectID);
 if ~exist(savePath,'dir')
@@ -8,43 +10,6 @@ if ~exist(savePath,'dir')
 end
 save(fullfile(savePath, [runName, '_voxelTimeSeries']), 'rawTimeSeriesPerVoxel', 'voxelIndices', '-v7.3');
 %% Clean time series from physio regressors
-
-physioRegressors = load(fullfile(functionalDir, [runName, '_puls.mat']));
-physioRegressors = physioRegressors.output;
-motionTable = readtable((fullfile(functionalDir, [runName, '_Movement_Regressors.txt'])));
-motionRegressors = table2array(motionTable(:,7:12));
-regressors = [physioRegressors.all, motionRegressors];
-
-% mean center these motion and physio regressors
-for rr = 1:size(regressors,2)
-    regressor = regressors(:,rr);
-    regressor = regressor - nanmean(regressor);
-    regressor = regressor ./ nanmean(regressor);
-    nanIndices = find(isnan(regressor));
-    regressor(nanIndices) = 0;
-    regressors(:,rr) = regressor;
-end
-
-% also add the white matter and ventricular time series
-%regressors(:,end+1) = meanTimeSeries.whiteMatter;
-%regressors(:,end+1) = meanTimeSeries.ventricles;
-    
-TR = 800;
-nFrames = 420;
-
-
-regressorsTimebase = 0:TR:nFrames*TR-TR;
-
-% remove all regressors that are all 0
-emptyColumns = [];
-for column = 1:size(regressors,2)
-    if ~any(regressors(:,column))
-        emptyColumns = [emptyColumns, column];
-    end
-end
-regressors(:,emptyColumns) = [];
-
-[ cleanedTimeSeriesPerVoxel, stats_physioMotionWMV ] = cleanTimeSeries( rawTimeSeriesPerVoxel, regressors, regressorsTimebase, 'meanCenterRegressors', false);
 
 
 
@@ -54,7 +19,7 @@ regressors(:,emptyColumns) = [];
 
 % pupil diameter
 regressors = [covariates.pupilDiameterConvolved; covariates.firstDerivativePupilDiameterConvolved];    
-[ ~, stats_pupilDiameter ] = cleanTimeSeries( cleanedTimeSeriesPerVoxel, regressors', covariates.pupilTimebase, 'meanCenterRegressors', true);
+[ ~, stats_pupilDiameter ] = cleanTimeSeries( rawTimeSeriesPerVoxel, regressors', covariates.pupilTimebase, 'meanCenterRegressors', true);
 [ pupilDiameter_beta ] = makeWholeBrainMap(stats_pupilDiameter.beta, voxelIndices, functionalScan);
 MRIwrite(pupilDiameter_beta, fullfile(getpref('mriTOMEAnalysis', 'TOME_analysisPath'), 'mriTOMEAnalysis', 'wholeBrain', 'resting', subjectID, [runName, '_pupilDiameter_beta.nii.gz']));
 [ pupilDiameter_pearsonR ] = makeWholeBrainMap(stats_pupilDiameter.pearsonR', voxelIndices, functionalScan);
