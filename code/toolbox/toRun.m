50f7ce8ef2ba399a5739c2fc07cb847ad88cefe0
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-17 16:09:11 -0500

    rerun TOME_3003

--- code/toolbox/toRun.m
+++ code/toolbox/toRun.m
@@ -2,6 +2,8 @@ analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1');
 analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2');
 analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3');
 analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4');
-debugBeta;
 
-
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
