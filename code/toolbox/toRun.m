83196202228fbdc4e04e173dfca71c1fa707b0c5
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-24 17:03:36 -0500

    do new wholeBrain routine

--- code/toolbox/toRun.m
+++ code/toolbox/toRun.m
@@ -1,9 +1,14 @@
-analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1');
-analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2');
-analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3');
-analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4');
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1');
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2');
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3');
+% analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4');
 
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
+
+analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
+analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
+analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
+analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
