ae90e1e90cbfd8a51eda51e7eeec694dc2803eea
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-28 16:30:38 -0500

    rerun first subject

--- code/toolbox/toRun.m
+++ code/toolbox/toRun.m
@@ -9,17 +9,17 @@
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
 
 analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
-
-analyzeWholeBrain('TOME_3005', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3005', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3005', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3005', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
-
-
-analyzeWholeBrain('TOME_3002', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3002', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3002', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3002', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
+% 
+% analyzeWholeBrain('TOME_3005', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3005', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3005', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3005', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
+% 
+% 
+% analyzeWholeBrain('TOME_3002', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3002', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
+% analyzeWholeBrain('TOME_3002', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
+%analyzeWholeBrain('TOME_3002', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
