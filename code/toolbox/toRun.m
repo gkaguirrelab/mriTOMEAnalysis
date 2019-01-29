8a105bbc68ea6a843f3beb49d13b5888d5f99675
harrison-mcadams <harrison.mcadams@gmail.com>
2019-01-29 13:02:10 -0500

    updated

--- code/toolbox/toRun.m
+++ code/toolbox/toRun.m
@@ -8,8 +8,8 @@
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
 % analyzeRest_wholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4_hp2000_clean', 'skipPhysioMotionWMVRegression', true)
 
-analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
-analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
+%analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run1', 'fileType', 'CIFTI');
+%analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run2', 'fileType', 'CIFTI');
 analyzeWholeBrain('TOME_3003', 'rfMRI_REST_AP_Run3', 'fileType', 'CIFTI');
 analyzeWholeBrain('TOME_3003', 'rfMRI_REST_PA_Run4', 'fileType', 'CIFTI');
 
