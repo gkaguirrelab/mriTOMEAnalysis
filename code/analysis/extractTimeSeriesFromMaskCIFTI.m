aee343f575bd24be7dcd2f71e0591cddbed772a8
harrison-mcadams <harrison.mcadams@gmail.com>
2019-02-21 16:43:03 -0500

    added mean center and central tendency options

--- code/analysis/extractTimeSeriesFromMaskCIFTI.m
+++ code/analysis/extractTimeSeriesFromMaskCIFTI.m
@@ -1,10 +1,29 @@
-function [ meanTimeSeries, timeSeriesPerRow] = extractTimeSeriesFromMaskCIFTI(mask, grayordinates)
+function [ meanTimeSeries, timeSeriesPerRow] = extractTimeSeriesFromMaskCIFTI(mask, grayordinates, varargin)
+
+%% Input Parser
+p = inputParser; p.KeepUnmatched = true;
+p.addParameter('meanCenter', true, @islogical);
+p.addParameter('whichCentralTendency', 'mean', @ischar);
+p.addParameter('saveName', [], @ischar);
+p.parse(varargin{:});
 
 % expand mask to be of the same dimension as the grayordinates
 expandedMask = repmat(mask, 1, size(grayordinates,2));
 
-timeSeriesPerRow = grayordinates .* timeSeriesPerRow;
+timeSeriesPerRow = grayordinates .* expandedMask;
 timeSeriesPerRow = timeSeriesPerRow(any(timeSeriesPerRow,2),:);
 
+if p.Results.meanCenter
+    timeSeriesPerRow = meanCenterTimeSeries(timeSeriesPerRow);
+end
+
+if strcmp(p.Results.whichCentralTendency, 'mean')    
+    meanTimeSeries = mean(timeSeriesPerRow,1);
+elseif strcmp(p.Results.whichCentralTendency, 'median')
+    meanTimeSeries = median(timeSeriesPerRow,1);
+elseif strcmp(p.Results.whichCentralTendency, 'PCA')
+    [coeffs] = pca(timeSeriesPerRow);
+    meanTimeSeries = coeffs(:,1);
+end
 
 end
