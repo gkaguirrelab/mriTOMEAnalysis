bd7441f44175c569aab5640c0f212547c9185366
harrison-mcadams <harrison.mcadams@gmail.com>
2019-02-21 14:50:07 -0500

    determine if there are any all NaN rows, which would result if the input was row was all 0

--- code/toolbox/meanCenterTimeSeries.m
+++ code/toolbox/meanCenterTimeSeries.m
@@ -12,4 +12,11 @@ dimrep(2) = dimsize;
 
 meanCenteredTimeSeries = (timeSeries - repmat(mean(timeSeries,2),dimrep))./repmat(mean(timeSeries,2),dimrep);
 
+% determine if there are any all NaN rows, which would result if the input
+% was row was all 0
+NaNRows = find(all(isnan(meanCenteredTimeSeries),2));
+for ii = 1:length(NaNRows)
+   meanCenteredTimeSeries(NaNRows(ii),:) = zeros(1, dimsize); 
+end
+
 end
