e7717994455c7ace0a6b007c34ccca17dad930e4
harrison-mcadams <harrison.mcadams@gmail.com>
2019-03-06 12:03:36 -0500

    added comments

--- code/toolbox/loadCIFTI.m
+++ code/toolbox/loadCIFTI.m
@@ -1,4 +1,28 @@
 function [ CIFTIMatrix ] = loadCIFTI(CIFTIFile, varargin)
+% Load CIFTI file into MATLAB as a matrix
+%
+% Syntax:
+%  [ CIFTIMatrix ] = loadCIFTI(CIFTIFile)
+%
+% Description:
+%  This routine converts the CIFTI into a text file, and then has MATLAB
+%  read in that textfile as a matrix. Each row of the matrix corresponds to
+%  a different grayordinate. This routine requires the HCP workbench
+%  package.
+%
+% Inputs:
+%  CIFTIFile             - a string that specifies the full path to the
+%                          CIFTI file to be loaded
+%
+% Optional key-value pairs:
+%  workbenchPath         - a string that defines the full path to where
+%                          workbench commands can be found.
+%
+% Outputs:
+%  grayordinates         - an m x n matrix, where m is the number of
+%                          grayordinates and n is the number of time points
+%                          in the acquisition (or whatever value, such as
+%                          R2 stashed for that grayordinate)
 
 %% Input parser
 p = inputParser; p.KeepUnmatched = true;
@@ -6,7 +30,7 @@ p.addParameter('workbenchPath', '/Applications/workbench/bin_macosx64/', @ischar
 p.parse(varargin{:});
 
 
-% load in smoothed gray-ordinate time series
+%% Load in smoothed gray-ordinate time series
 % So we can load it into MATLAB
 system(['bash ', p.Results.workbenchPath, 'wb_command -cifti-convert -to-text ', CIFTIFile, ' ', [CIFTIFile(1:end-9), '.txt']]);
 
