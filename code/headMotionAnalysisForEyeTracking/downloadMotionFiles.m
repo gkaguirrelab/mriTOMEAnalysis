3ed0205fd8c75df1323cbc46db8df2d480cad8ef
gkaguirre <gkaguirre@me.com>
2019-09-29 15:37:13 -0400

    Make this a function
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/headMotionAnalysisForEyeTracking/downloadMotionFiles.m
+++ code/headMotionAnalysisForEyeTracking/downloadMotionFiles.m
@@ -1,3 +1,4 @@
+function downloadMotionFiles(sessionIdx)
 % downloadMotionFiles.m
 %
 % This script identifies all the analyses in the tome project that contain
@@ -6,6 +7,14 @@
 % result save directory.
 %
 
+switch sessionIdx
+    case 1
+        sessionLabelPrefix = {'Session 1','Session 1a','Session 1b'};
+        sessionLabelReplacement = {'session1_restAndStructure','session1_restAndStructure','session1_restAndStructure'};
+    case 2
+        sessionLabelPrefix = {'Session 2','Session 2a','Session 2b'};
+        sessionLabelReplacement = {'session2_restAndStructure','session2_restAndStructure','session2_restAndStructure'};
+end
 
 projectName = 'tome';
 gearName = 'hcp-func';
@@ -13,10 +22,6 @@ scratchSaveDir = getpref('flywheelMRSupport','flywheelScratchDir');
 resultSaveDirStem = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/TOME_analysis/deriveCameraPositionFromHeadMotion';
 outputFileSuffix = '_hcpfunc.zip';
 resultFileSuffix = {'Movement_Regressors.txt','Scout_gdc.nii.gz','DistortionCorrectionAndEPIToT1wReg_FLIRTBBRAndFreeSurferBBRbased/EPItoT1w.dat'};
-sessionLabelPrefix = {'Session 1','Session 1a','Session 1b'};
-%sessionLabelPrefix = {'Session 2','Session 2a','Session 2b'};
-sessionLabelReplacement = {'session1_restAndStructure','session1_restAndStructure','session1_restAndStructure'};
-%sessionLabelReplacement = {'session2_restAndStructure','session2_restAndStructure','session2_restAndStructure'};
 
 devNull = ' >/dev/null';
 
@@ -36,28 +41,28 @@ searchStruct = struct(...
     struct('match', struct('project0x2elabel', projectName))
     }} ...
     );
-analyses = fw.search(searchStruct, 'size', '1000');
+analyses = fw.search(searchStruct, 'size', 1000);
 
 %% Loop through the analyses and download
-for ii = 537:numel(analyses)
-        
+for ii = 1:numel(analyses)
+    
     % Get the analysis object
     thisAnalysis = fw.getAnalysis(analyses{ii}.analysis.id);
-
+    
     % Get the session info for this analysis
     thisSession = fw.getSession(thisAnalysis.parent.id);
     sessionLabelIdx = startsWith(thisSession.label,sessionLabelPrefix);
-
+    
     % This is not the session you are looking for: move along, move along.
     if sum(sessionLabelIdx)==0
         continue
     end
-
+    
     % More than one session matches this stem.
     if sum(sessionLabelIdx)>1
         error('This subject has more than one session of this type that contains this analysis');
     end
-
+    
     % Find the file with the matching stem
     analysisFileMatchIdx = cellfun(@(x) endsWith(x.name,outputFileSuffix),thisAnalysis.files);
     
@@ -66,7 +71,7 @@ for ii = 537:numel(analyses)
         warning('There are either zero or more than one files found for this analysis; skipping');
         continue
     end
-        
+    
     % Get the subject ID, session label, and study date
     thisSubject = thisSession.subject.code;
     thisSessionLabel = thisSession.label;
@@ -77,7 +82,7 @@ for ii = 537:numel(analyses)
     thisName = thisAnalysis.files{analysisFileMatchIdx}.name;
     tmp = strsplit(thisName,outputFileSuffix);
     saveStem = tmp{1};
-
+    
     % Check to see if we have already processed this session
     checkFileName = fullfile(resultSaveDirStem,sessionLabelReplacement{sessionLabelIdx},[saveStem '*' ]);
     if ~isempty(dir(checkFileName))
@@ -85,25 +90,25 @@ for ii = 537:numel(analyses)
         fprintf([reportLineOut '\n']);
         continue
     end
-
+    
     % Time the loop from here
     timerVal = tic;
-
+    
     % Download the matching file to the rootSaveDir. This can take a while
     zipFileName = fw.downloadOutputFromAnalysis(thisAnalysis.id,thisName,fullfile(scratchSaveDir,thisName));
-
+    
     % Unzip the downloaded file; overwright existing; pipe the terminal
     % output to dev/null
     command = ['unzip -o -a ' zipFileName ' -d ' zipFileName '_unzip' devNull];
     system(command);
-
+    
     % Save a .mat file with session label, study date, and subject
     sessionInfo.subject = thisSubject;
     sessionInfo.session = thisSessionLabel;
-    sessionInfo.date = thisSessionDate;    
+    sessionInfo.date = thisSessionDate;
     destinationFile = fullfile(resultSaveDirStem,sessionLabelReplacement{sessionLabelIdx},[saveStem '_sessionInfo.mat']);
     save(destinationFile,'sessionInfo');
-
+    
     % Copy the result files to the resultsSaveDir. If there are
     % duplicates of the target file, just copy the first.
     for jj=1:length(resultFileSuffix)
@@ -113,12 +118,12 @@ for ii = 537:numel(analyses)
         destinationFile = fullfile(resultSaveDirStem,sessionLabelReplacement{sessionLabelIdx},[saveStem '_' targetFiles(1).name]);
         copyfile(sourceFile,destinationFile);
     end
-        
+    
     % Delete the downloaded files in the scratch dir
     rmdir([zipFileName '_unzip'], 's');
     delete(zipFileName);
     
-    % Record the time    
+    % Record the time
     minutesPassed = toc(timerVal)/60;
     
     % Report completion of this step
@@ -126,3 +131,4 @@ for ii = 537:numel(analyses)
     fprintf([reportLineOut ' - %2.1f mins \n'],minutesPassed);
 end
 
+end % Main function
