5d14eec0892f19a734f3ce08ac73854d3199dc79
gkaguirre <gkaguirre@me.com>
2019-11-03 10:21:36 -0500

    Script to gather the HRF params from the flobHRF model across tome subjects
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/submitGears/extractHRFParams.m
@@ -0,0 +1,45 @@
+% Script to extract and report the flobsHRF median HRF parameters across
+% the TOME subjects.
+
+
+%% Instantiate the flywheel object
+fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
+
+% Get a list of all completed flobsHRF models on the forwardModel gear
+jobList = fw.jobs.find(...
+    'state=complete',...
+    'gear_info.name=forwardmodel',...
+    'config.config.modelClass="flobsHRF"');
+
+% Create a temporary directory to save files
+outDir = tempdir;
+
+% Loop through the analyses. Can't figure out how to get the flywheel api
+% to let me know if an analysis still exists or not, so right now I'm doing
+% this in a try-catch frame (yuck).
+for jj = 1:length(jobList)
+    try
+        analysisHandle = fw.getAnalysis(jobList{jj}.destination.id);
+    catch
+        continue
+    end
+    
+    % Check if this is a tome project analysis
+    if strcmp(analysisHandle.parents.group,'tome')
+        
+        % Find the results.mat file and download
+        fileIdx = find(cellfun(@(x) strcmp(x.type,'MATLAB data'),analysisHandle.files));
+        fileName = analysisHandle.files{fileIdx}.name;
+        outPath = fullfile(outDir,fileName);
+        fw.downloadOutputFromAnalysis(analysisHandle.id,fileName,outPath);
+        
+        % Load the mat file into memory
+        load(outPath,'results')
+        
+        % Report this result to the screen
+        fprintf([fileName ' - (hrfParams),[%2.4f,%2.4f,%2.4f]\n'],results.summary.medianParams)
+        
+    end
+    
+end
+
