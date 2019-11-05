103d48dd9e034fa8c0bf5a2d168ba0422f8946ab
gkaguirre <gkaguirre@me.com>
2019-11-05 11:03:27 -0500

    Handle bad jobs
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/submitGears/downloadAnalysisFiles.m
+++ code/submitGears/downloadAnalysisFiles.m
@@ -1,22 +1,33 @@
 % Script to download analysis files
 
-gearName = 'forwardmodel';
-modelClass = 'prfTimeShift';
+% gearName = 'forwardmodel';
+% modelClass = 'prfTimeShift';
+% outDir = '/Users/aguirre/Desktop/bayesPRFPlots';
+% fileType = 'image';
+
+gearName = 'bayesprf';
+modelClass = '';
+outDir = '/Users/aguirre/Desktop/bayesPRFPlots';
+
+% Leave one of these empty
 fileType = 'image';
-
-% Where to save the files
-outDir = '/Users/aguirre/Desktop/prfTimeShiftPlots';
-
+fileName = '';
 
 %% Instantiate the flywheel object
 fw = flywheel.Flywheel(getpref('flywheelMRSupport','flywheelAPIKey'));
 
 % Get a list of all completed flobsHRF models on the forwardModel gear
-jobList = fw.jobs.find(...
-    'state=complete',...
-    ['gear_info.name=' gearName],...
-    ['config.config.modelClass="' modelClass '"']);
 
+if ~isempty(modelClass)
+    jobList = fw.jobs.find(...
+        'state=complete',...
+        ['gear_info.name=' gearName],...
+        ['config.config.modelClass="' modelClass '"']);
+else
+    jobList = fw.jobs.find(...
+        'state=complete',...
+        ['gear_info.name=' gearName]);
+end
 
 % Loop through the analyses. Can't figure out how to get the flywheel api
 % to let me know if an analysis still exists or not, so right now I'm doing
@@ -28,15 +39,24 @@ for jj = 1:length(jobList)
         continue
     end
     
+    % If there are no files in this analysis, continue
+    if isempty(analysisHandle.files)
+        continue
+    end
+    
     % Check if this is a tome project analysis
     if strcmp(analysisHandle.parents.group,'tome')
         
         % Download the files
-        fileIdx = find(cellfun(@(x) strcmp(x.type,fileType),analysisHandle.files));
+        if ~isempty(fileName)
+            fileIdx = find(cellfun(@(x) contains(x.name,fileName),analysisHandle.files));
+        else
+            fileIdx = find(cellfun(@(x) strcmp(x.type,fileType),analysisHandle.files));
+        end
         for pp = 1:length(fileIdx)
-            fileName = analysisHandle.files{fileIdx(pp)}.name;
-            outPath = fullfile(outDir,fileName);
-            fw.downloadOutputFromAnalysis(analysisHandle.id,fileName,outPath);
+            outName = analysisHandle.files{fileIdx(pp)}.name;
+            outPath = fullfile(outDir,outName);
+            fw.downloadOutputFromAnalysis(analysisHandle.id,outName,outPath);
         end
         
     end
