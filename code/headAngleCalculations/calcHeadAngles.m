5d1d9ae6f04541ea281754797c90f9474ecc87bf
Briana Haggerty <eddaha@alumni.upenn.edu>
2021-01-27 15:43:01 -0500

    skip images without ROIs

--- code/headAngleCalculations/calcHeadAngles.m
+++ code/headAngleCalculations/calcHeadAngles.m
@@ -53,10 +53,11 @@ for ii = 1:numel(sessions)
     file = acquisition.files{idxFile};
     
     % Grab the ROI structures
+    if isfield(file.info, 'roi') == 1
     rois = file.info.roi;
     
-    % Need to go into the elements of the structure here to figure out
-    % which ones are which
+    Need to go into the elements of the structure here to figure out
+    which ones are which
     for annot = 1:numel(rois)
        if isequal(rois(annot).description,'midline falx')
            xStart = rois(annot).handles.start.x;
@@ -67,5 +68,5 @@ for ii = 1:numel(sessions)
            rois(annot).interhemisphericFissureAngle = angle;
        end
     end
-    
+    end
 end
