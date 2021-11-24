a84926c2d0e9fbd4b1393597a36a60aaae2843a4
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-11-24 04:56:17 -0500

    A file downloader script so we don't have to push inner ear stuff to our git

---
+++ gear_tomeInnerEarAngles/build_gear_image.py
@@ -0,0 +1,21 @@
+import os
+
+# Specify version that will be appended to the Docker image name
+version = '0.3.1'
+
+# Get the current directory 
+current_folder = os.getcwd()
+
+# Download supplements
+os.system(f'wget https://www.dropbox.com/s/wdelnnxndgana0e/innerEarSupplements_v_0_1_3.zip?dl=0 -P {current_folder}')
+
+# Get the zip file 
+zip_file = os.path.join(current_folder, 'innerEarSupplements_v_0_1_3.zip?dl=0')
+
+# Unzip it and delete the zip file
+os.system(f'unzip {zip_file} -d {current_folder}')
+os.system(f'rm {zip_file}')
+
+# Docker build 
+docker_file = os.path.join(current_folder, 'Dockerfile')
+os.system(f'docker build -t gkaguirrelab/tome-calculate-inner-ear-angles:{version} {current_folder}/.')
