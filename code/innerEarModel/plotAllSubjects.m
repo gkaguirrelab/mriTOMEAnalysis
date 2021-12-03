d285fd8e892ce8928f9ee331d5115cca47c352f0
ozenc <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2021-12-03 00:38:47 -0500

    Making the first part of the direction fixes for LMU dataset

--- code/innerEarModel/plotAllSubjects.m
+++ code/innerEarModel/plotAllSubjects.m
@@ -7,118 +7,69 @@ allNormals = {};
 for ii = 1:length(folders)
     [lateralMRILeft, lateralMRIRight, anteriorMRILeft, anteriorMRIRight, ...
      posteriorMRILeft, posteriorMRIRight] = loadMRINormals(fullfile(folders(ii).folder,folders(ii).name));
- 
-    if strcmp(folders(ii).name, 'TOME_3001')
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3002')
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3003')    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;    
-    elseif strcmp(folders(ii).name, 'TOME_3004')    
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3005')    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3007')    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3008')    
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3009') 
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;    
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3011') 
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;   
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;    
-    elseif strcmp(folders(ii).name, 'TOME_3012') 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3013') 
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;  
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;   
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;        
-    elseif strcmp(folders(ii).name, 'TOME_3014') 
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;           
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3015')  
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3017')  
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;    
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3018') 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;     
-    elseif strcmp(folders(ii).name, 'TOME_3019') 
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;  
-    elseif strcmp(folders(ii).name, 'TOME_3020')   
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;     
-    elseif strcmp(folders(ii).name, 'TOME_3022')  
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3024')  
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;    
-    elseif strcmp(folders(ii).name, 'TOME_3025')    
-        anteriorMRIRight.normal = -anteriorMRIRight.normal; 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;        
-    elseif strcmp(folders(ii).name, 'TOME_3026') 
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;     
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;    
-    elseif strcmp(folders(ii).name, 'TOME_3027')  
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;          
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;     
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3028')  
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;  
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3029') 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;  
-    elseif strcmp(folders(ii).name, 'TOME_3030') 
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;
-    elseif strcmp(folders(ii).name, 'TOME_3031')    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal; 
-    elseif strcmp(folders(ii).name, 'TOME_3032') 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3033')
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;       
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;  
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;     
-    elseif strcmp(folders(ii).name, 'TOME_3034')   
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;     
-    elseif strcmp(folders(ii).name, 'TOME_3035') 
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;  
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;         
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3036')  
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;  
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;   
-    elseif strcmp(folders(ii).name, 'TOME_3037')    
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;   
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;      
-    elseif strcmp(folders(ii).name, 'TOME_3039')  
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;  
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;            
-    elseif strcmp(folders(ii).name, 'TOME_3040')  
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;   
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;    
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;    
-    elseif strcmp(folders(ii).name, 'TOME_3042')    
-        anteriorMRILeft.normal = -anteriorMRILeft.normal; 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;  
-    elseif strcmp(folders(ii).name, 'TOME_3043')    
-        anteriorMRILeft.normal = -anteriorMRILeft.normal;   
-        posteriorMRILeft.normal = -posteriorMRILeft.normal;         
-        anteriorMRIRight.normal = -anteriorMRIRight.normal;
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;        
-    elseif strcmp(folders(ii).name, 'TOME_3044')   
-        posteriorMRILeft.normal = -posteriorMRILeft.normal; 
-        posteriorMRIRight.normal = -posteriorMRIRight.normal; 
-    elseif strcmp(folders(ii).name, 'TOME_3045')  
-        posteriorMRIRight.normal = -posteriorMRIRight.normal;        
-    end        
-    
+
+    if contains(downloadFolder, 'tome')    
+        if strcmp(folders(ii).name, 'TOME_3001') || strcmp(folders(ii).name, 'TOME_3008') || strcmp(folders(ii).name, 'TOME_3011') || strcmp(folders(ii).name, 'TOME_3019') || strcmp(folders(ii).name, 'TOME_3037') || strcmp(folders(ii).name, 'TOME_3042')
+            anteriorMRILeft.normal = -anteriorMRILeft.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;        
+        elseif strcmp(folders(ii).name, 'TOME_3002') || strcmp(folders(ii).name, 'TOME_3003') || strcmp(folders(ii).name, 'TOME_3005') || strcmp(folders(ii).name, 'TOME_3007') || strcmp(folders(ii).name, 'TOME_3012') || strcmp(folders(ii).name, 'TOME_3015') || strcmp(folders(ii).name, 'TOME_3018') || strcmp(folders(ii).name, 'TOME_3020') || strcmp(folders(ii).name, 'TOME_3022') || strcmp(folders(ii).name, 'TOME_3029') || strcmp(folders(ii).name, 'TOME_3031') || strcmp(folders(ii).name, 'TOME_3032') || strcmp(folders(ii).name, 'TOME_3034') || strcmp(folders(ii).name, 'TOME_3045') 
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;
+        elseif strcmp(folders(ii).name, 'TOME_3004') || strcmp(folders(ii).name, 'TOME_3025') || strcmp(folders(ii).name, 'TOME_3036')     
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;
+        elseif strcmp(folders(ii).name, 'TOME_3009') || strcmp(folders(ii).name, 'TOME_3017')
+            posteriorMRILeft.normal = -posteriorMRILeft.normal;    
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal; 
+        elseif strcmp(folders(ii).name, 'TOME_3013') || strcmp(folders(ii).name, 'TOME_3028') || strcmp(folders(ii).name, 'TOME_3033') || strcmp(folders(ii).name, 'TOME_3035') || strcmp(folders(ii).name, 'TOME_3040') 
+            anteriorMRILeft.normal = -anteriorMRILeft.normal;  
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;   
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;         
+        elseif strcmp(folders(ii).name, 'TOME_3014') || strcmp(folders(ii).name, 'TOME_3026') || strcmp(folders(ii).name, 'TOME_3039') || strcmp(folders(ii).name, 'TOME_3044')
+            posteriorMRILeft.normal = -posteriorMRILeft.normal;           
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;    
+        elseif strcmp(folders(ii).name, 'TOME_3024')  
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;  
+        elseif strcmp(folders(ii).name, 'TOME_3027')  
+            anteriorMRILeft.normal = -anteriorMRILeft.normal;          
+            posteriorMRILeft.normal = -posteriorMRILeft.normal;     
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;     
+        elseif strcmp(folders(ii).name, 'TOME_3030') 
+            posteriorMRILeft.normal = -posteriorMRILeft.normal;
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;                       
+        elseif strcmp(folders(ii).name, 'TOME_3043')    
+            anteriorMRILeft.normal = -anteriorMRILeft.normal;   
+            posteriorMRILeft.normal = -posteriorMRILeft.normal;         
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;            
+        end       
+    elseif contains(downloadFolder, 'dataset-1')
+        if strcmp(folders(ii).name, 'sub-001')
+            anteriorMRILeft.normal = -anteriorMRILeft.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;   
+        elseif strcmp(folders(ii).name, 'sub-002')   
+            posteriorMRILeft.normal = -posteriorMRILeft.normal;     
+        elseif strcmp(folders(ii).name, 'sub-003') || strcmp(folders(ii).name, 'sub-004') || strcmp(folders(ii).name, 'sub-008') || strcmp(folders(ii).name, 'sub-013') || strcmp(folders(ii).name, 'sub-017') || strcmp(folders(ii).name, 'sub-018') || strcmp(folders(ii).name, 'sub-023') || strcmp(folders(ii).name, 'sub-024')  
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;            
+        elseif strcmp(folders(ii).name, 'sub-005')   
+            anteriorMRILeft.normal = -anteriorMRILeft.normal;   
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;   
+        elseif strcmp(folders(ii).name, 'sub-009') 
+            posteriorMRILeft.normal = -posteriorMRILeft.normal; 
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;
+            posteriorMRIRight.normal = -posteriorMRIRight.normal; 
+        elseif strcmp(folders(ii).name, 'sub-011') 
+            posteriorMRILeft.normal = -posteriorMRILeft.normal; 
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;  
+        elseif strcmp(folders(ii).name, 'sub-014') || strcmp(folders(ii).name, 'sub-026')    
+            posteriorMRIRight.normal = -posteriorMRIRight.normal;
+            anteriorMRIRight.normal = -anteriorMRIRight.normal;      
+        end
+    elseif contains(downloadFolder, 'dataset-2')
+        x = 'not filled';
+    end
+
     allNormals{ii,1} = lateralMRILeft;
     allNormals{ii,2} = lateralMRIRight;
     allNormals{ii,3} = anteriorMRILeft;
@@ -147,26 +98,49 @@ for nn = 1:length(allNormals)
     averagePosteriorRight.normal = averagePosteriorRight.normal + allNormals{nn,6}.normal;
 end
 
-averageLateralLeft.normal = averageLateralLeft.normal/43;
-averageLateralRight.normal = averageLateralRight.normal/43;
-averageAnteriorLeft.normal = averageAnteriorLeft.normal/43;
-averageAnteriorRight.normal = averageAnteriorRight.normal/43;
-averagePosteriorLeft.normal = averagePosteriorLeft.normal/43;
-averagePosteriorRight.normal = averagePosteriorRight.normal/43;
-
+if contains(downloadFolder, 'tome')
+    averageLateralLeft.normal = averageLateralLeft.normal/43;
+    averageLateralRight.normal = averageLateralRight.normal/43;
+    averageAnteriorLeft.normal = averageAnteriorLeft.normal/43;
+    averageAnteriorRight.normal = averageAnteriorRight.normal/43;
+    averagePosteriorLeft.normal = averagePosteriorLeft.normal/43;
+    averagePosteriorRight.normal = averagePosteriorRight.normal/43;
+elseif contains(downloadFolder, 'dataset-1')
+    averageLateralLeft.normal = averageLateralLeft.normal/16;
+    averageLateralRight.normal = averageLateralRight.normal/16;
+    averageAnteriorLeft.normal = averageAnteriorLeft.normal/16;
+    averageAnteriorRight.normal = averageAnteriorRight.normal/16;
+    averagePosteriorLeft.normal = averagePosteriorLeft.normal/16;
+    averagePosteriorRight.normal = averagePosteriorRight.normal/16;
+elseif contains(downloadFolder, 'dataset-2')
+    averageLateralLeft.normal = averageLateralLeft.normal/70;
+    averageLateralRight.normal = averageLateralRight.normal/70;
+    averageAnteriorLeft.normal = averageAnteriorLeft.normal/70;
+    averageAnteriorRight.normal = averageAnteriorRight.normal/70;
+    averagePosteriorLeft.normal = averagePosteriorLeft.normal/70;
+    averagePosteriorRight.normal = averagePosteriorRight.normal/70;
+end    
+    
 calcAP = rad2deg(atan2(norm(cross(averageAnteriorLeft.normal,averagePosteriorLeft.normal)), dot(averageAnteriorLeft.normal,averagePosteriorLeft.normal)));
 calcAL = rad2deg(atan2(norm(cross(averageAnteriorLeft.normal,averageLateralLeft.normal)), dot(averageAnteriorLeft.normal,averageLateralLeft.normal)));
 calcPL = rad2deg(atan2(norm(cross(averagePosteriorLeft.normal,averageLateralLeft.normal)), dot(averagePosteriorLeft.normal,averageLateralLeft.normal)));
 fprintf(['Calculated angle between Anterior and Posterior Left ear is ' num2str(calcAP) '\n'])
 fprintf(['Calculated angle between Anterior and Lateral Left ear is ' num2str(calcAL) '\n'])
-fprintf(['Calculated angle between Posterior and Lateral Left ear is ' num2str(calcPL) '\n'])
+fprintf(['Calculated angle between Posterior and Lateral Left ear is ' num2str(calcPL) '\n\n'])
 
 calcAP = rad2deg(atan2(norm(cross(averageAnteriorRight.normal,averagePosteriorRight.normal)), dot(averageAnteriorRight.normal,averagePosteriorRight.normal)));
 calcAL = rad2deg(atan2(norm(cross(averageAnteriorRight.normal,averageLateralRight.normal)), dot(averageAnteriorRight.normal,averageLateralRight.normal)));
 calcPL = rad2deg(atan2(norm(cross(averagePosteriorRight.normal,averageLateralRight.normal)), dot(averagePosteriorRight.normal,averageLateralRight.normal)));
 fprintf(['Calculated angle between Anterior and Posterior Right ear is ' num2str(calcAP) '\n'])
 fprintf(['Calculated angle between Anterior and Lateral Right ear is ' num2str(calcAL) '\n'])
-fprintf(['Calculated angle between Posterior and Lateral Right ear is ' num2str(calcPL) '\n'])
+fprintf(['Calculated angle between Posterior and Lateral Right ear is ' num2str(calcPL) '\n\n'])
+
+calcAP = rad2deg(atan2(norm(cross(averageAnteriorRight.normal,averageAnteriorLeft.normal)), dot(averageAnteriorRight.normal,averageAnteriorLeft.normal)));
+calcAL = rad2deg(atan2(norm(cross(averageLateralRight.normal,averageLateralLeft.normal)), dot(averageLateralRight.normal,averageLateralLeft.normal)));
+calcPL = rad2deg(atan2(norm(cross(averagePosteriorRight.normal,averagePosteriorLeft.normal)), dot(averagePosteriorRight.normal,averagePosteriorLeft.normal)));
+fprintf(['Calculated angle between Anterior left-right ear is ' num2str(calcAP) '\n'])
+fprintf(['Calculated angle between Lateral left-right ear is ' num2str(calcAL) '\n'])
+fprintf(['Calculated angle between Posterior left-right ear is ' num2str(calcPL) '\n\n'])
 
 figure
 plotMRINormals(averageLateralLeft, averageLateralRight, ...
