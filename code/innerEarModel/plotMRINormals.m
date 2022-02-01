ad507cc94cc3e613aabdf241ab6850d7b0917557
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2022-02-01 00:08:19 -0500

    Plot color changes

--- code/innerEarModel/plotMRINormals.m
+++ code/innerEarModel/plotMRINormals.m
@@ -14,7 +14,7 @@ function plotMRINormals(lateralMRILeft, lateralMRIRight, ...
         ylabel('y')
         zlabel('z')
         quiver3(-3,0,0,anteriorMRILeft(1),anteriorMRILeft(2),anteriorMRILeft(3),'g','LineStyle', '--')
-        quiver3(-3,0,0,posteriorMRILeft(1),posteriorMRILeft(2),posteriorMRILeft(3),'b','LineStyle', '--')
+        quiver3(-3,0,0,posteriorMRILeft(1),posteriorMRILeft(2),posteriorMRILeft(3),'y','LineStyle', '--')
 
         % Plot the right ear
         quiver3(0,0,0,lateralMRIRight(1),lateralMRIRight(2),lateralMRIRight(3),'r','LineStyle', '--')
@@ -34,7 +34,7 @@ function plotMRINormals(lateralMRILeft, lateralMRIRight, ...
         ylabel('y')
         zlabel('z')
         quiver3(-3,0,0,anteriorMRILeft(1),anteriorMRILeft(2),anteriorMRILeft(3),'g','linewidth',5,'color', [0.4660 0.6740 0.1880])
-        quiver3(-3,0,0,posteriorMRILeft(1),posteriorMRILeft(2),posteriorMRILeft(3),'b','linewidth',5,'color', [0 0 0.5])
+        quiver3(-3,0,0,posteriorMRILeft(1),posteriorMRILeft(2),posteriorMRILeft(3),'y','linewidth',5,'color', [155 135 12]/255)
 
         % Plot the right ear
         quiver3(0,0,0,lateralMRIRight(1),lateralMRIRight(2),lateralMRIRight(3),'r','linewidth',5, 'color', [0.6350 0.0780 0.1840])
@@ -43,7 +43,7 @@ function plotMRINormals(lateralMRILeft, lateralMRIRight, ...
         zlabel('z')
         axis equal
         quiver3(0,0,0,anteriorMRIRight(1),anteriorMRIRight(2),anteriorMRIRight(3),'g','linewidth',5, 'color', [0.4660 0.6740 0.1880])
-        quiver3(0,0,0,posteriorMRIRight(1),posteriorMRIRight(2),posteriorMRIRight(3),'b','linewidth',5,'color', [0 0 0.5])
+        quiver3(0,0,0,posteriorMRIRight(1),posteriorMRIRight(2),posteriorMRIRight(3),'y','linewidth',5,'color', [155 135 12]/255)
         hold off
 
     else 
@@ -54,7 +54,7 @@ function plotMRINormals(lateralMRILeft, lateralMRIRight, ...
         ylabel('y')
         zlabel('z')
         quiver3(-3,0,0,anteriorMRILeft(1),anteriorMRILeft(2),anteriorMRILeft(3),'g')
-        quiver3(-3,0,0,posteriorMRILeft(1),posteriorMRILeft(2),posteriorMRILeft(3),'b')
+        quiver3(-3,0,0,posteriorMRILeft(1),posteriorMRILeft(2),posteriorMRILeft(3),'y')
 
         % Plot the right ear
         quiver3(0,0,0,lateralMRIRight(1),lateralMRIRight(2),lateralMRIRight(3),'r')
@@ -63,6 +63,6 @@ function plotMRINormals(lateralMRILeft, lateralMRIRight, ...
         zlabel('z')
         axis equal
         quiver3(0,0,0,anteriorMRIRight(1),anteriorMRIRight(2),anteriorMRIRight(3),'g')
-        quiver3(0,0,0,posteriorMRIRight(1),posteriorMRIRight(2),posteriorMRIRight(3),'b')
+        quiver3(0,0,0,posteriorMRIRight(1),posteriorMRIRight(2),posteriorMRIRight(3),'y')
         hold off    
     end
