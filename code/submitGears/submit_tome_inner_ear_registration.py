18cc6f1ab23653697d66a4406ceae1de01f09206
ozenc <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2022-02-16 01:12:00 -0500

    Only submit the inner ear gear if T2 exists in the first session

--- code/submitGears/submit_tome_inner_ear_registration.py
+++ code/submitGears/submit_tome_inner_ear_registration.py
@@ -1,9 +1,12 @@
 import flywheel
 import datetime
 
+# Enter flywheel key here
+flywheelKey = ''
+
 # Initialize gear stuff
 now = datetime.datetime.now().strftime("%y/%m/%d_%H:%M")
-fw = flywheel.Client('upenn.flywheel.io:DTIiZcuXBVlpJmCLZt')
+fw = flywheel.Client(flywheelKey)
 proj = fw.projects.find_first('label=tome')
 subjects = proj.subjects()
 qp = fw.lookup('gears/tome-calculate-inner-ear-angles')
@@ -16,26 +19,27 @@ for sub in subjects:
     subjID = sub.label
     sessions = sub.sessions()
     for ses in sessions:
-        acquisitions = ses.acquisitions()
-        for acq in acquisitions:
-            if 'T2w' in acq.label:
-                if sub.label == 'TOME_3039' and ses.label == 'Session 1':
-                    print('skipping TOME_3039 session 1 as T2 is discarded')
-                elif sub.label == 'TOME_3030' and ses.label == 'Session 2':
-                    print('skipping TOME_3030 session 2 as T2 is discarded')                
-                elif sub.label == 'TOME_3044' and ses.label == 'Session 2':
-                    print('skipping TOME_3044 session 2 as T2 is discarded')  
-                elif sub.label == 'TOME_3029' and ses.label == 'Session 1':
-                    print('skipping TOME_3029 session 1 as T2 is discarded')       
-                elif sub.label == 'TOME_3046' and ses.label == 'Session 1':
-                    print('skipping TOME_3046 session 1 as T2 is discarded') 
-                else:    
-                    image_container = acq
-                    for ni in image_container.files:
-                        if 'nii.gz' in ni.name:
-                            nifti_image = ni
-                            config['subject_id'] = subjID
-                            inputs['T1_or_T2_image'] = nifti_image
-                            _id = qp.run(analysis_label=analysis_label, config=config, 
-                                          inputs=inputs, destination=ses, tags=['vm-n1-highmem-8_disk-1500G_swap-60G'])
-                            print('submitting inner ear gear for %s' % (subjID))
+        if ses.label == 'Session 1':
+            acquisitions = ses.acquisitions()
+            for acq in acquisitions:
+                if 'T2w' in acq.label:
+                    if sub.label == 'TOME_3039' and ses.label == 'Session 1':
+                        print('skipping TOME_3039 session 1 as T2 is discarded')
+                    elif sub.label == 'TOME_3030' and ses.label == 'Session 2':
+                        print('skipping TOME_3030 session 2 as T2 is discarded')                
+                    elif sub.label == 'TOME_3044' and ses.label == 'Session 2':
+                        print('skipping TOME_3044 session 2 as T2 is discarded')  
+                    elif sub.label == 'TOME_3029' and ses.label == 'Session 1':
+                        print('skipping TOME_3029 session 1 as T2 is discarded')       
+                    elif sub.label == 'TOME_3046' and ses.label == 'Session 1':
+                        print('skipping TOME_3046 session 1 as T2 is discarded') 
+                    else:    
+                        image_container = acq
+                        for ni in image_container.files:
+                            if 'nii.gz' in ni.name:
+                                nifti_image = ni
+                                config['subject_id'] = subjID
+                                inputs['T1_or_T2_image'] = nifti_image
+                                _id = qp.run(analysis_label=analysis_label, config=config, 
+                                              inputs=inputs, destination=ses, tags=['vm-n1-highmem-8_disk-1500G_swap-60G'])
+                                print('submitting inner ear gear for %s' % (subjID))
